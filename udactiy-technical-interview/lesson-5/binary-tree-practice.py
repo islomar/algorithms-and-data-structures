453670c9c93b1bf8827008b9284776f7446983a5
Isidro López <islomar@gmail.com>
2018-02-18 19:07:45 +0100

    [udacity] Binary Tree practice: solution from Udacity (I like mine more :-) )

--- udactiy-technical-interview/lesson-5/binary-tree-practice.py
+++ udactiy-technical-interview/lesson-5/binary-tree-practice.py
@@ -22,8 +22,7 @@ class BinaryTree(object):
         """Print out all tree nodes
         as they are visited in
         a pre-order traversal."""
-        initial_traversal = []
-        return self.preorder_print(self.root, initial_traversal)
+        return self.preorder_print(self.root, [])
 
     def preorder_search(self, start, find_val):
         """Helper method - use this to create a
@@ -42,6 +41,32 @@ class BinaryTree(object):
         if start.right is not None:
             self.preorder_print(start.right, traversal)
         return '-'.join(traversal)
+
+
+class BinaryTreeFromUdacity(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def search(self, find_val):
+        return self.preorder_search(tree.root, find_val)
+
+    def print_tree(self):
+        return self.preorder_print(tree.root, "")[:-1]
+
+    def preorder_search(self, start, find_val):
+        if start:
+            if start.value == find_val:
+                return True
+            else:
+                return self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val)
+        return False
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
 
 
 # Set up tree
