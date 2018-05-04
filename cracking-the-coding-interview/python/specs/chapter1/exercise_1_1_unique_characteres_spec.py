71d8634f583141644c48441bf243bbd48a131be8
Isidro López <islomar@gmail.com>
2018-05-04 23:29:41 +0200

    [CtCi] [chapter 1] Detect all unique characters NOT using any additional data structure

--- cracking-the-coding-interview/python/specs/chapter1/exercise_1_1_unique_characteres_spec.py
+++ cracking-the-coding-interview/python/specs/chapter1/exercise_1_1_unique_characteres_spec.py
@@ -15,8 +15,22 @@ def has_all_unique_characters(my_string=None):
     return True
 
 
+# Worst case (time) = ~O(2*n - 1) = O(n)
+def has_all_unique_characters_v2(my_string=None):
+    if not my_string:
+        return True
+    index = 1
+    for character in my_string:
+        rest = my_string[index:]
+        for another_character in rest:
+            if character == another_character:
+                return False
+        index += 1
+    return True
+
+
 with describe('Chapter 1'):
-    with context('Has all unique characters'):
+    with context('Has all unique characters (using additional data structures)'):
         with it('returns true for no String'):
             expect(has_all_unique_characters()).to(be_true)
 
@@ -31,3 +45,19 @@ with describe('Chapter 1'):
 
         with it('returns false for abcda'):
             expect(has_all_unique_characters("abcda")).to(be_false)
+
+    with context('Has all unique characters (not using additional data structures)'):
+        with it('returns true for no String'):
+            expect(has_all_unique_characters_v2()).to(be_true)
+
+        with it('returns true for empty String'):
+            expect(has_all_unique_characters_v2("")).to(be_true)
+
+        with it('returns true for abc'):
+            expect(has_all_unique_characters_v2("abc")).to(be_true)
+
+        with it('returns false for aa'):
+            expect(has_all_unique_characters_v2("aa")).to(be_false)
+
+        with it('returns false for abcda'):
+            expect(has_all_unique_characters_v2("abcda")).to(be_false)
