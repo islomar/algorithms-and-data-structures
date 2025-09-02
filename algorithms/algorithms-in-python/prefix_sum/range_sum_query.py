91cc380a7fb5a62cb66e275d815a0edce7060a67
Isidro Lopez <islomar@gmail.com>
2025-09-02 18:49:49 +0200

    [algorithms-in-python] Solve range sum query

--- algorithms/algorithms-in-python/prefix_sum/range_sum_query.py
+++ algorithms/algorithms-in-python/prefix_sum/range_sum_query.py
@@ -1,5 +1,29 @@
+# https://leetcode.com/problems/range-sum-query-immutable/description/
 from typing import List
 
 
-def fix_me() -> int:
-    return 42
+class NumArray:
+
+    def __init__(self, elements: List[int]):
+        self._elements = elements
+        self._precomputed_sums = self._initialize_precomputed_sums(elements)
+
+    @staticmethod
+    def _initialize_precomputed_sums(elements: List[int]):
+        precomputed_sums = []
+        temp_sum = 0
+        for element in elements:
+            temp_sum += element
+            precomputed_sums.append(temp_sum)
+        return precomputed_sums
+
+    def sum_range(self, left: int, right:int) -> int:
+        result: int = 0
+        for i in range(left, right + 1):
+            result += self._elements[i]
+        return result
+
+    def sum_range_improved(self, left: int, right:int) -> int:
+        if left == 0:
+            return self._precomputed_sums[right]
+        return self._precomputed_sums[right] - self._precomputed_sums[left - 1]
--- algorithms/algorithms-in-python/prefix_sum/test_range_sum_query.py
+++ algorithms/algorithms-in-python/prefix_sum/test_range_sum_query.py
@@ -1,5 +1,28 @@
-from prefix_sum.range_sum_query import fix_me
+from expects import equal, expect
+
+from prefix_sum.range_sum_query import NumArray
+
+## Solutions
+# https://leetcode.com/problems/range-sum-query-immutable/solutions/7050842/prefix-sum-range-sum-query-python-java-t-c8rr/
 
 
-def test_fix_me():
-    assert fix_me() == 41
+def test_sum_range() -> None:
+    numArray = NumArray([-2, 0, 3, -5, 2, -1])
+
+    result = numArray.sum_range(0, 2)
+
+    expect(result).to(equal(1))
+
+def test_sum_range_2() -> None:
+    numArray = NumArray([-2, 0, 3, -5, 2, -1])
+
+    result = numArray.sum_range(2, 5)
+
+    expect(result).to(equal(-1))
+
+def test_sum_range_improved() -> None:
+    numArray = NumArray([-2, 0, 3, -5, 2, -1])
+
+    result = numArray.sum_range_improved(0, 2)
+
+    expect(result).to(equal(1))
